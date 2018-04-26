a0af832cbbdb3cf8a6c4e56a44a8739499482617
Pola Malak <PolaMalak96@gmail.com>
2018-04-26 06:49:05 +0200

    Add environment and Characters

---
+++ Shoot em!/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    
+
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRb;
+    int floorMask;
+    float camRayLength = 100f;
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
+    }
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime * 6.0f;
+        playerRb.MovePosition(transform.position + movement);
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRb.MoveRotation(newRotation);
+        }
+    }
+    void Animating(float h,float v)
+    {
+            bool Run = h != 0f || v != 0f;
+        anim.SetBool("IsRunning", Run);
+    }
+
+}
+

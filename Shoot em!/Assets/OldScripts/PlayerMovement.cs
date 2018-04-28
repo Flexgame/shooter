319f7f67bbdec1609dd6f620d3d1016c5d3d4eb8
Pola Malak <PolaMalak96@gmail.com>
2018-04-28 04:10:12 +0200

    Edit Scripts
    Edit conrollers Scripts

--- Shoot em!/Assets/OldScripts/PlayerMovement.cs
+++ Shoot em!/Assets/OldScripts/PlayerMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    
-
     public float speed = 6f;
     Vector3 movement;
     Animator anim;
@@ -26,34 +24,29 @@ public class PlayerMovement : MonoBehaviour {
         Move(h, v);
         Turning();
         Animating(h, v);
-
     }
     void Move(float h, float v)
     {
         movement.Set(h, 0f, v);
-        movement = movement.normalized * speed * Time.deltaTime * 8.0f;
+        movement = movement.normalized * speed * Time.deltaTime;
         playerRb.MovePosition(transform.position + movement);
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
     }
     void Turning()
     {
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit floorHit;
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
             Vector3 playerToMouse = floorHit.point - transform.position;
             playerToMouse.y = 0f;
             Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
             playerRb.MoveRotation(newRotation);
         }
     }
-    void Animating(float h,float v)
+    void Animating(float h, float v)
     {
-            bool Run = h != 0f || v != 0f;
-        anim.SetBool("IsRunning", Run);
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
     }
 
 }
